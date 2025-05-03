def1da58ddfb92318f69d299e12ba267c56aada5
beka <beqacaava1997@gmail.com>
2025-05-03 17:30:41 +0400

    9

--- js/main.js
+++ js/main.js
@@ -1,12 +1,21 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const langSelect = document.getElementById("langSelect");
+  loadLanguage(langSelect.value);
 
-function setLanguage(lang) {
+  langSelect.addEventListener("change", (e) => {
+    loadLanguage(e.target.value);
+  });
+});
+
+function loadLanguage(lang) {
   fetch(`lang/${lang}.json`)
     .then(res => res.json())
     .then(data => {
+      // Set text content from JSON
       for (const key in data) {
         const el = document.getElementById(key);
         if (el) el.textContent = data[key];
       }
-    });
+    })
+    .catch(err => console.error("Error loading language file:", err));
 }
-window.onload = () => setLanguage('en');
